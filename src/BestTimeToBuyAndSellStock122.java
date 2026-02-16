f8127fe936b560b8b371eded27525db6c74dd5b3
golisairam <sairamgoli0564@gmail.com>
2026-02-16 21:59:25 +0530

    Buy sell stock we can buy and sell the stock many times needed but need to sell before buying and retun the all the profits

---
+++ src/BestTimeToBuyAndSellStock122.java
@@ -0,0 +1,16 @@
+public class BestTimeToBuyAndSellStock122 {
+    public int maxProfit(int[] prices) {
+        int profit =0;
+        for(int i = 1;i<prices.length;i++){
+            if(prices[i] > prices[i-1]){
+                profit += prices[i] - prices[i-1];
+            }
+        }
+        return profit;
+    }
+}
+// Input: prices = [7,1,5,3,6,4]
+//Output: 7
+//Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+//Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+//Total profit is 4 + 3 = 7.
