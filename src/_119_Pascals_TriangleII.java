8054a67ed64a51cb0d4abc35a9554156ab408ffc
golisairam <sairamgoli0564@gmail.com>
2026-03-04 22:58:01 +0530

    modified

--- src/_119_Pascals_TriangleII.java
+++ src/_119_Pascals_TriangleII.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
+import java.util.*;
 public class _119_Pascals_TriangleII {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> row = new ArrayList<>();
+        long value = 1;
+        row.add((int)value);
+        for(int i = 1;i<=rowIndex;i++){
+            value = value * (rowIndex - i + 1);
+            value /= i;
+            row.add((int)value);
+        }
+        return row;
+    }
 }
