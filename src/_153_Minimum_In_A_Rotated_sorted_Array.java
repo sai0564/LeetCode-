1d03e63f707deab4513a787ab6a71e3af07f1ba7
golisairam <sairamgoli0564@gmail.com>
2026-03-14 19:01:39 +0530

    optimized

--- src/_153_Minimum_In_A_Rotated_sorted_Array.java
+++ src/_153_Minimum_In_A_Rotated_sorted_Array.java
@@ -1,10 +1,19 @@
 public class _153_Minimum_In_A_Rotated_sorted_Array {
-        public int findMin(int[] nums) {
-            int min =Integer.MAX_VALUE;
-            for (int i = 0; i < nums.length; i++) {
-                int temp = nums[i];
-                min = Math.min(temp,min);
-            }
-            return min ;
+        public int findMin(int[] arr) {
+                int low = 0;
+                int high = arr.length -1;
+                int min = Integer.MAX_VALUE;
+                while(low<=high){
+                    int mid = low + (high-low)/2;
+                    if(arr[low]<=arr[mid]){
+                        min =Math.min(min,arr[low]);
+                        low = mid +1;
+                    }
+                    else{
+                        min =Math.min(min,arr[mid]);
+                        high = mid - 1;
+                    }
+                }
+                return min;
         }
 }
