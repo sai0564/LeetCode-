7426e6ca44c8ede275f378d13bbb732ea48f7b1f
golisairam <sairamgoli0564@gmail.com>
2026-03-15 22:46:02 +0530

    using xor we can find the unique element we xor the element not index

---
+++ src/_540_Single_Element_in_a_Sorted_Array.java
@@ -0,0 +1,9 @@
+public class _540_Single_Element_in_a_Sorted_Array {
+    public int singleNonDuplicate(int[] nums) {
+        int xor = 0;
+        for (int i = 0; i < nums.length; i++) {
+            xor = xor ^ nums[i];
+        }
+        return xor;
+    }
+}
