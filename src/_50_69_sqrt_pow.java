13468f0c1eef4b3741a5112ead9dd27a1610cff8
golisairam <sairamgoli0564@gmail.com>
2026-03-19 20:21:58 +0530

    for streak

---
+++ src/_50_69_sqrt_pow.java
@@ -0,0 +1,17 @@
+public class _50_69_sqrt_pow {
+        public double myPow(double x, int n) {
+            double ans = Math.pow(x,n);
+            return ans;
+        }
+
+
+   /* class Solution {
+        public int mySqrt(int x) {
+            double temp = Math.sqrt(x);
+            double ans = Math.floor(temp);
+            return (int)ans;
+        }
+
+    */
+
+}
